937caa315e7e4ff63e7d50b9fad3f0ed4297f232
Andrey Sychev <rekreg@bk.ru>
2020-02-07 01:57:26 +0300

    Уровень 3: День 1: 0:52:26

--- oop-test.php
+++ oop-test.php
@@ -3,20 +3,52 @@
 class Pet {
   public $name;
   public $age = 10;
+  public $type;
   
   function say($w) {
-    echo "Object said $w";
+    echo "{$this->name} said $w <br>";
   }
+  
+  function functionName(){
+    echo "<p>Вызвана функция ".__FUNCTION__."<br>";
+  }
+  
+  function className(){
+    echo "<p>Используем класс ".__CLASS__."<br>";
+  }
+  
+  function methodName(){
+    echo "<p>Вызван метод ".__METHOD__."<br>";
+  }
+  
+  function __construct($type) {
+    $this->type = $type;
+    echo "{$type} СОЗДАН! <br>";
+  }
+  
+  function __destruct() {
+    //$this->type = $type;
+    echo "Удален! <br>";
+    
+  }
+  
+   
+  
 
 }
 
-$cat = new Pet();
-$dog = new Pet();
+$cat = new Pet("cat");
+$dog = new Pet("dog");
 
 
 $cat->name = "Марта";
+//echo $cat->name;
+$dog->name = "Тузик";
+$dog->say("Gav");
 
-echo $cat->name;
+$dog->functionName();
+$dog->className();
+$dog->methodName();
 
 
 
