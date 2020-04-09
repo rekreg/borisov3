b710838a861333ed22ea2a31533898d5672e87c5
Andrey Sychev <rekreg@bk.ru>
2020-04-10 02:49:22 +0300

    Уровень 3: День 3: 0:20:36

--- news/rss_reader.php
+++ news/rss_reader.php
@@ -1,5 +1,23 @@
 <?php
 
+
+const RSS_URL = "http://localhost:8080/borisov3/news/rss.xml";
+const FILE_NAME = "news.xml";
+const RSS_TTL = 3600;
+
+
+function download($url, $filename) {
+  $file = file_get_contents($url);
+  if($file) file_put_contents($filename, $file);
+}
+
+
+  if(!is_file(FILE_NAME))
+    download(RSS_URL, FILE_NAME);
+
+
+
+
 ?>
 <!DOCTYPE html>
 
@@ -12,7 +30,30 @@
 
 <h1>Последние новости</h1>
 <?php
+  
+  $xml = simplexml_load_file(FILE_NAME);
 
+
+  foreach($xml->channel->item as $news) {
+    
+    echo "<hr>";
+    echo $news->title;
+    echo "<br>";
+    echo $news->link;
+    echo "<br>";
+    echo $news->category;
+    echo "<br>";
+    echo $news->pubDate;
+    echo "<br>";
+    echo $news->description;
+    echo "<hr>";
+   
+    }
+  
+  if(time() > filemtime(FILE_NAME)+RSS_TTL )
+    download(RSS_URL, FILE_NAME);
+  
 ?>
 </body>
 </html>
+
